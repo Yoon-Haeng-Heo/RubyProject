f915426a6156f83faf7a87526e809055d55bc325
Yoon-Haeng-Heo <submailid96@gmail.com>
2020-08-19 16:51:02 +0900

    finish delete

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root 'home#index'
+  
+  get 'home/index'
+  
+  get 'home/new'
+
+  post 'home/create'
+  
+  get 'home/destroy/:post_id' =>'home#destroy'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
