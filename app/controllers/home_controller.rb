9804ad7745c3253e0793ddbd1294b8369893c35e
Yoon-Haeng-Heo <submailid96@gmail.com>
2020-08-20 13:53:14 +0900

    finish CRUD

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -20,4 +20,15 @@ class HomeController < ApplicationController
     @post.destroy
     redirect_to "/"
   end
+  def edit
+    @post = Post.find(params[:post_id])
+  end
+  def update    
+    post = Post.find(params[:post_id])
+    post.title = params[:post_title]
+    post.content = params[:post_content]
+    post.save
+
+    redirect_to "/"
+  end
 end
