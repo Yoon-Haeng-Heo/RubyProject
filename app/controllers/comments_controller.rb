a6d5ffad0f98d17a16135be652dc64b498b83096
Yoon-Haeng-Heo <submailid96@gmail.com>
2020-08-24 17:57:08 +0900

    add delete only comment user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
   def create
     comment = Comment.new(comment_params)
     comment.post_id = params[:post_id]
+    comment.user_id = current_user.id  ## 현재 유저 정보를 가져오는 것
     comment.save
 
     redirect_back fallback_location: root_path
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_075427) do
+ActiveRecord::Schema.define(version: 2020_08_24_084409) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
     t.integer "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -41,4 +43,5 @@ ActiveRecord::Schema.define(version: 2020_08_24_075427) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
 end
